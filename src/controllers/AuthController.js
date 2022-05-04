b5ee2fd8286c0d01c4106d961e30dec999655cbc
Shabangu Thabiso <shabanguthabiso02@gmail.com>
2022-05-04 13:19:48 +0200

    Auth complted with Mail and Pass Reset Token - complete auth system

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,20 +1,26 @@
 import connection from '../database/connection'
 import { genSalt, hash, compare } from "bcryptjs"
 import { sign } from 'jsonwebtoken'
-import { validateCreate, validateLogin, validatePassword, validateEmail } from '../validation/AuthValidation'
+import Joi from 'joi'
+import Mail from '../mail/Mail'
 
 class AuthController {
     // create new user
     create(request, response) {
         // Validate request body Input | name | email | password |
-        const { error } = validateCreate(request.body)
-        if (error) return response.status(422).send({ message: error.details[0].message })
+        const schema = Joi.object({
+            name: Joi.string().required().min(3).max(50),
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(8),
+        })
+        const { error } = schema.validate(request.body)
+        if (error) return response.status(422).send(error.details[0].message)
 
         // Check if email Exits
         const statement = `SELECT * FROM users WHERE email=?`
         connection.execute(statement, [ request.body.email ], async(error, results) => {
-            if (error) return response.status(500).send({ message: "Internal Server Error" })
-            if (results.length > 0) return response.status(422).send({ message: "Sorry email already exists" })
+            if (error) return response.status(500).send("Internal Server Error")
+            if (results.length > 0) return response.status(422).send("Sorry! email already exists")
 
             // Generate a Hashed Password
             const salt = await genSalt(10)
@@ -27,28 +33,42 @@ class AuthController {
                 hashedPassword
             ]
             const statement = `INSERT INTO users(name, email, password) VALUES(?, ?, ?)`
-            connection.execute(statement, data, (error) => {
-                if (error) return response.status(500).send({ message: "Internal Server Error" })
-                response.status(201).send({ message:  `${request.body.name} your account have been created` })
+            connection.execute(statement, data, async(error) => {
+                if (error) return response.status(500).send("Internal Server Error")
+                const mail_template = {
+                    name: request.body.name,
+                    email: request.body.email,
+                    subject: 'welcome to the pos stystems',
+                    text: 'welcome to the pos stystems and this is the body of the email'
+                }
+                const info = await Mail(mail_template)
+                response.status(201).send({
+                    message:  `${request.body.name} your account have been created`,
+                    mail: info
+                })
             })
         })
     }
 
     login(request, response) {
-        // Login Validation request body | email | password |
-        const { error } = validateLogin(request.body)
-        if (error) return response.status(422).send({ message: error.details[0].message })
+        // Login Validation request body | email | password
+        const schema = Joi.object({
+            email: Joi.string().required().email(),
+            password: Joi.string().required().min(8),
+        })
+        const { error } = schema.validate(request.body)
+        if (error) return response.status(422).send(error.details[0].message)
 
         const statement = `SELECT id, name, password, active, email_verified FROM users WHERE email=?`
 
         connection.execute(statement, [ request.body.email ], async(error, results) => {
-            if (error) return response.status(500).send({ message: "Internal Server Error" })
-            if (results.length < 1) return response.status(422).send({ message: "Sorry! this user does not exist" })
+            if (error) return response.status(500).send("Internal Server Error")
+            if (results.length < 1) return response.status(422).send("Sorry! this user does not exist")
             // validate if account is active 
-            if (!results[0].active) return response.status(401).send({ message: "Account in not active - see management" })
+            if (!results[0].active) return response.status(401).send("Account in not active - contact management")
             // validate password
             const validPass = await compare(request.body.password, results[0].password)
-            if(!validPass) return response.status(422).send({ message: "Sorry! user password is incorrect" })
+            if(!validPass) return response.status(422).send("Sorry! user password is incorrect")
             
             const user = {
                 id: results[0].id,
@@ -63,29 +83,42 @@ class AuthController {
     }
 
     sendPasswordResetEmail(request, response) {
-        const { error } = validateEmail(request.body)
-        if (error) return response.status(422).send({ message: error.details[0].message })
+        const schema = Joi.object({
+            email: Joi.string().required().email(),
+        })
+        const { error } = schema.validate(request.body)
+        if (error) return response.status(422).send(error.details[0].message)
 
         // Check if email Exits
         const statement = `SELECT id, name, email FROM users WHERE email=?`
         connection.execute(statement, [ request.body.email ], async(error, results) => {
-            if (error) return response.status(500).send({ message: "Internal Server Error" })
-            if (results.length < 1) return response.status(404).send({ message: "Sorry! Pleace enter your correct email address" })
+            if (error) return response.status(500).send("Internal Server Error")
+            if (results.length < 1) return response.status(404).send("Sorry! Pleace enter your correct email address")
             
             const user = results[0]
-            const token = sign({id: user.id}, process.env.JWT_PASSWORD_RESET, { expiresIn: '60min'})
-
-            /**
-             * Sign a Password Reset JWT token and send an email to user
-             * jwt sing JWT_PASSWORD_RESET
-             */
-            response.send({ message: "password reset email sent", resetTokenTest: token })
+            const reset_token = sign({id: user.id}, process.env.JWT_PASSWORD_RESET, { expiresIn: '1d'})
+            // Sign a Password Reset JWT token and send an email to user
+            const mail_template = {
+                name: results[0].name,
+                email: request.body.email,
+                subject: 'welcome to the pos stystems',
+                text: 'welcome to the pos stystems and this is the body of the email'
+            }
+            const info = await Mail(mail_template)
+            response.send({
+                message: "password reset email sent",
+                reset_token,
+                mail: info
+            })
         })
     }
 
     async passwordReset(request, response) {
-        const { error } = validatePassword(request.body)
-        if (error) return response.status(422).send({ message: error.details[0].message })
+        const schema = Joi.object({
+            password: Joi.string().required().min(8),
+        })
+        const { error } = schema.validate(request.body)
+        if (error) return response.status(422).send(error.details[0].message)
 
         // Generate a Hashed Password
         const salt = await genSalt(10)
@@ -94,7 +127,7 @@ class AuthController {
 
         const statement = `UPDATE users SET password=? WHERE id=?`
         connection.execute(statement, data, (error) => {
-            if (error) return response.status(500).send({ message: "Internal Server Error" })
+            if (error) return response.status(500).send("Internal Server Error")
             response.send({ message: "Your password have been updated"})
         })
     }
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import Token from "../middleware/Token"
+import Mail from '../mail/Mail'
 
 // Import controllers
 import AuthController from "../controllers/AuthController"
@@ -12,15 +13,15 @@ const router = Router()
 // auth routes
 router.post('/oauth/login', AuthController.login)
 router.post('/oauth/register', AuthController.create)
-router.post('/oauth/verify', Token.verify, (request, response) =>{
-    response.status(200).send('authoried')
-})
+router.post('/oauth/forgot', AuthController.sendPasswordResetEmail)
+router.put('/oauth/reset', Token.verifyPasswordReset, AuthController.passwordReset)
+router.post('/oauth/verify', Token.verify, (request, response) => response.sendStatus(200))
 
 //  Check out routes
 router.get('/product/:barcode', Token.verify, CheckController.get_product_by_barcode)
 router.post('/checkout', Token.verify, CheckController.checkout)
 
-// users routes
+// get currrent login user route
 router.get('/user', Token.verify, UsersController.get_current_user)
 
 export default router
